38f0946e820e0fa1cd79d31c87c5993b434da420
zeustata <zeustata@users.noreply.github.com>
2026-05-22 01:23:52 +0200

    Add PWA support (manifest, sw, icon, install banner)

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'porra-cache-v1';
+const urlsToCache = [
+  '/porra/',
+  '/porra/index.html',
+  '/porra/icon.svg',
+  '/porra/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
