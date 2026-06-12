f2c38832779f62f766c106b77429d0cf7c575052
zeustata <zeustata@users.noreply.github.com>
2026-06-13 00:29:23 +0200

    Update Lode predictions and bump SW cache version

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'porra-cache-v52';
+const CACHE_NAME = 'porra-cache-v53';
 const ASSETS = [
     './',
     './index.html',
-    './css/style.css?v=49',
+    './css/style.css?v=50',
     './logo.png',
     './manifest.json',
-    './js/api-engine.js?v=49',
-    './data/participants.json?v=49',
+    './js/api-engine.js?v=50',
+    './data/participants.json?v=50',
     './data/official_answers.json',
     './reglas.html'
 ];
