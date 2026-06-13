912b2d580b230d067d3242d60ffbb1c4c1f4660d
zeustata <zeustata@users.noreply.github.com>
2026-06-14 00:35:05 +0200

    Actualizar respuestas oficiales y subir cache sw.js a v62

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'porra-cache-v61';
+const CACHE_NAME = 'porra-cache-v62';
 const ASSETS = [
     './',
     './index.html',
-    './css/style.css?v=61',
+    './css/style.css?v=62',
     './logo.png',
     './manifest.json',
-    './js/api-engine.js?v=61',
-    './data/participants.json?v=61',
+    './js/api-engine.js?v=62',
+    './data/participants.json?v=62',
     './data/official_answers.json',
     './reglas.html'
 ];
