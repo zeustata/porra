c85a31f23916a044f264d533a0732688a79972a6
zeustata <zeustata@users.noreply.github.com>
2026-06-17 09:16:35 +0200

    Actualizar respuestas oficiales y subir cache a v70

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'porra-cache-v69';
+const CACHE_NAME = 'porra-cache-v70';
 const ASSETS = [
     './',
     './index.html',
-    './css/style.css?v=69',
+    './css/style.css?v=70',
     './logo.png',
     './manifest.json',
-    './js/api-engine.js?v=69',
-    './data/participants.json?v=69',
+    './js/api-engine.js?v=70',
+    './data/participants.json?v=70',
     './data/official_answers.json',
     './reglas.html'
 ];
